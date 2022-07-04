2f73a28f70a5464913a7fdb2dc1b72d1ac8f96a3
Wongi <hpurs95@gmail.com>
2022-07-04 21:51:37 +0900

    Initial Commit

---
+++ Function Types/Function_TypesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Function_TypesApp.swift
+//  Function Types
+//
+//  Created by MacBook Air on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct Function_TypesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
